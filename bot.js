700aaae694a5b671c29db7e165515ef90e9bac01
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-13 02:28:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -131,6 +131,27 @@ client.on("message", msg => {
       msg.channel.send({embed: embed})
   }
 });
+client.on('message', message => {
+  var prefix = "$";
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  if (command == "kick") {
+      if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply('You Dont Have **KICK_MEMBERS** Permission!');
+        var member= message.mentions.members.first();
+        member.kick().then((member) => {
+            message.channel.send(member.displayName + " Kicked From " + message.guild.name);
+            message.channel.send("By: " + "<@" + message.author.id + ">")
+            message.channel.sendMessage(`تم حفظ السبب وستتم مراجعته من قبل الأداره`)
+client.channels.get(`466615358690820096`).sendMessage("** تم طرد هذا الشخص من قبل " + message.guild.owner + " سبب مذكور **" + args.join("  "))
+        }).catch(() => {
+            message.channel.send(`:x: I cant kick this member`);
+        });
+    }
+});
 
 
 
