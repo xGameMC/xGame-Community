29194aba87d8c7f4ca36bb180b19a5430db58cad
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-13 02:52:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -167,30 +167,5 @@ return message.reply("**:white_check_mark: .. تم فك الميوت عن الش
 };
 
 
- 
-
-client.on("message", message => {
-              var args = message.content.substring(prefix.length).split(" ");
-              if (message.content.startsWith(prefix + "clear")) {
-                  if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
-     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**⚠  لا يوجد لديك صلاحية لمسح الشات**');
-          var msg;
-          msg = parseInt();
-        
-        message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-        message.channel.sendMessage("", {embed: {
-          title: "``تــم مسح الشات ``",
-          color: 0x06DF00,
-          footer: {
-            
-          }
-        }}).then(msg => {msg.delete(3000)});
-                            }
-  
-       
-  });
-
-
-
 
 client.login(process.env.BOT_TOKEN);
