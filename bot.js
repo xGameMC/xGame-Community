e83f6de220e4c6cbb2fb05074948765537a4d828
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-12 22:04:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,34 +27,7 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-lient.on("message", message => {
-  let command = message.content.split(" ")[0];
-  if (command === "$mute") {
-          if(!message.channel.guild) return message.reply(':x: اسف لكن هذا الامر للسيرفرات فقط ');
-                  if (!message.member.hasPermission('MANAGE_ROLES')) return message.reply(" لا يوجد لديك برمشن 'Manage Roles' ");
-  let user = message.mentions.users.first();
-  let modlog = client.channels.find('name', 'console');
-  let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
-  if (!muteRole) return message.reply(" لا يوجد رتبة الميوت 'Muted' ").catch(console.error);
-  if (!modlog) return message.reply("لا يوجد الروم المراد ارسال المعلومات له 'Mute-Log'");
-  if (message.mentions.users.size < 1) return message.reply(' يجب عليك المنشن اولاً ');
-  const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .addField(' Mute ', ' | :white_check_mark: |')
-.addField('تم اعطاء الميوت ل',${user.username}#${user.discriminator})
-    .addField('السبب', 'تعكير نظام الشات')
-    .addField('بواسطة:', ${message.author.username}#${message.author.discriminator})
-   message.channel.send({embed: embed});
 
-  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply(' لا يوجد لدي برمشن Manage Roles ').catch(console.error);
-
-  if (message.guild.member(user).roles.has(muteRole.id)) {
-      client.channels.get(modlog.id).send({embed}).catch(console.error);
-  } else {
-    message.guild.member(user).addRole(muteRole).then(() => {
-      client.channels.get(modlog.id).send({embed}).catch(console.error);
-    });
-  }
 
 };
 
