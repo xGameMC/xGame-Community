3f47dccbb4caffb71e0e2ec30174a8b0fe3eee2b
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-13 03:41:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,7 +47,65 @@ client.user.setGame(`xGameMC`,"http://twitch.tv/S-F")
       }}).then(msg => {msg.delete(3000)});
                           }
 
-     
+    
+    
+    
+        if(message.content === prefix + "$open chat") {
+                        if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
+              message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: true
+
+              }).then(() => {
+                  message.reply("**__تم فتح الشات__:white_check_mark:**")
+              });
+                }
+                
+         
+       
+});
+
+
+
+client.on('message', message => {
+
+       if(message.content === prefix + "$close chat") {
+                           if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
+              message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: false
+
+              }).then(() => {
+                  message.reply("**__تم تقفيل الشات__ :white_check_mark: **")
+              });
+                }
+  
+  
+  
+   client.on('message' , message => {
+
+    if (message.content === "$invite") {
+        if(!message.channel.guild) return message.reply('**الآمر فقط في السيرفرات**');
+     const embed = new Discord.RichEmbed()
+ .setColor("RANDOM")
+ .setThumbnail(client.user.avatarURL)     
+ .setDescription("Add me" + `
+ **
+رابط البوت | https://discord.gg/rknN5TE
+ **
+`);
+  message.author.sendEmbed(embed);
+   }
+});
+  
+  
+  
+  
+
+
+    
 }); 
 
 client.login(process.env.BOT_TOKEN);
