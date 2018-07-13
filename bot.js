220d37b8a08d29004c8732014a2ae38efe90d442
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-13 03:04:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,30 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "$clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```but any number of messeges u want to remove ??```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\ndone i removed: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+
+
 
 client.login(process.env.BOT_TOKEN);
