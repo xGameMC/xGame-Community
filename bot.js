5fba4b7afd7f4bde28fad7e1e73caf46720bc9a2
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-13 03:02:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,55 +27,5 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-const Discord = require("discord.js");
-const fs = require("fs");
-const client = new Discord.Client();
-//جاكيو لفل 
-let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
-const prefix = "$level";
-
-client.on("message", message => {
-  if (!message.content.startsWith(prefix)) return;
-  if (message.author.bot) return;
-
-  if (!points[message.author.id]) points[message.author.id] = {
-    points: 0,
-    level: 0
-  };
-  let userData = points[message.author.id];
-  userData.points++;
-
-  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
-  if (curLevel > userData.level) {
-    // اللفل الجديد
-    userData.level = curLevel;
-     message.reply(`**لقد وصلت الى المستوى ${curLevel}**`).then(m => m.delete(100000));
-  }
-
-  if (message.content.startsWith(prefix + "level")) {
-    // الفل
-      message.reply(` ** انت في المستوى ${userData.level}  مع ${userData.points} نقاط . ** `).then(m => m.delete(100000));
-
-  }
-  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
-    if (err) console.error(err)
-  });
-  client.on('message', message => {
-    if (message.content.startsWith("*avatar")) {
-        var mentionned = message.mentions.users.first();
-    var x5bzm;
-      if(mentionned){
-          var x5bzm = mentionned;
-      } else {
-          var x5bzm = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setImage(`${x5bzm.avatarURL}`)
-      message.channel.sendEmbed(embed);
-    }
-});
-  
 
 client.login(process.env.BOT_TOKEN);
