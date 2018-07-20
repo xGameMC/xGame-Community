0b35b8ee254f58ea63548f298df2e3461dbc284b
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-20 03:30:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ client.on('message', message => {
 
    
 client.on('message', eyad => {
-  if (eyad.content.startsWith('mute')) {
+  if (eyad.content.startsWith('$mute')) {
 if (!eyad.member.hasPermission("MOVE_MEMBERS")) return eyad.channel.send("**لا يمكنك كتابه الامر الاتي*** | ❎ ");
 let men = eyad.mentions.users.first()
 let mas = eyad.author
@@ -113,6 +113,51 @@ client.on("guildMemberAdd", function(member) {
         
 });
 
+```client.on('message', msg => {//msg
+    if (msg.content === 'الوان') {
+      if (msg.channel.id !== "ايدي الروم اللي تبي يستقبل الامر منه") return;
+      msg.channel.send({file : "https://d.top4top.net/p_925trmdy1.png"})
+    }
+  });;
+  
+const Client = require('discord.js');
+
+client.on('message', message => {
+    let args = message.content.split(' ').slice(1);
+if(message.content.split(' ')[0] == 'لون'){
+if (message.channel.id !== "ايدي الروم اللي تبيه يستقبل الامر منه") return;
+     const embedd = new Discord.RichEmbed()
+.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+.setDescription(`**There's No Color With This Number ** ❌ `)
+.setColor(`ff0000`)
+
+if(!isNaN(args) && args.length > 0)
+
+
+ var a = message.guild.roles.find("name",`${args}`)
+          if(!a)return;
+          if (a.name > 250 || a.name < 1) return;
+const embed = new Discord.RichEmbed()
+              
+.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+.setDescription(`**Color Changed Successfully** ✅ `)
+
+.setColor(`${a.hexColor}`)
+message.channel.sendEmbed(embed);
+    if (!args)return;
+setInterval(function(){})
+            let count = 0;
+            let ecount = 0;
+  for(let x = 1; x < 201; x++){
+     
+      message.member.removeRole(message.guild.roles.find("name",`${x}`))
+    
+      }
+          message.member.addRole(message.guild.roles.find("name",`${args}`));
+  
+      
+}
+});
 
 
 
