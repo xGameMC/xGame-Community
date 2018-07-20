bd8887cf7fc26885e6750621c31315c47396572a
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-20 04:16:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ client.on('message', message => {
    
 
 client.on("guildMemberAdd", function(member) {
-    const wc = member.guild.channels.find("name", "member-log")
+    const wc = member.guild.channels.find("name", "》welcome《")
         const embed = new Discord.RichEmbed()
         .setColor('B90C0C')
         .setAuthor(member.user.tag, member.user.avatarURL)
