e1ef1656332d4a9c368e099f6aec5dbda36c7154
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-07-20 05:32:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -210,39 +210,7 @@ client.on("message", message => {
   });
 
 
-client.on('message', async message => {
 
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
-
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "warn$") { //??? ???????
-
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-
-  if (message.mentions.users.size < 1) return message.reply("**???? ???**");
-  if(!reason) return message.reply ("**???? ??? ?????**");
-
-
-  if(!warns[user.id]) warns[user.id] = {
-    warns: 0
-  };
-
-  warns[user.id].warns++;
-
-  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
-    if (err) console.log(err)
-  
-	  
-	  
+ 	  
   
 client.login(process.env.BOT_TOKEN);
