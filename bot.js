9aab723f20afad1cfda150c9dc99558eb2d5420c
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-08-04 11:13:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -240,7 +240,7 @@ client.on('message', message =>{
   if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("لا يمكنك كتابة الامر الاتي");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("يجب عليك وضع المنشن");
-  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
+  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("ا يمكنك كتابة الامر الاتي");
   let reason = args.join(" ").slice(22);
 
   if(!warns[wUser.id]) warns[wUser.id] = {
