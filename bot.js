90cf4b3ea738a779c576ce3d9c0e49c7fde4b5f9
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-08-05 10:56:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -282,34 +282,20 @@ client.on('message', message => {
     }
 });
 	
-	client.on('message', message => { 
-    var prefix ="$";
-           if (message.content.startsWith(prefix + "user")) {
-     var args = message.content.split(" ").slice(1);
-     let user = message.mentions.users.first();
-     var men = message.mentions.users.first();
-        var heg;
-        if(men) {
-            heg = men
-        } else {
-            heg = message.author
-        }
-      var mentionned = message.mentions.members.first();
-         var h;
-        if(mentionned) {
-            h = mentionned
-        } else {
-            h = message.member
-        }
-      var id = new  Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL) 
-    .setColor("#707070")
-    .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true
-    .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true             
-    .setFooter(`Morro Bot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
-    message.channel.send(id)
-}       });
-	
-});
+const child_process = require("child_process");
+const adminprefix = "$";
+const devs = ['426823056418406418'];
+
+client.on('message', message => {
+if(message.content === adminprefix + "rest") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
+        console.log(⚠️ جاري اعادة تشغيل البوت... ⚠️);
+        client.destroy();
+        child_process.fork(__dirname + "/الملف.js");
+        console.log(تم اعادة تشغيل البوت);
+    }
+  
+  });
 
 client.login(process.env.BOT_TOKEN);
