284767af5c76abd4bb3374693328b7fc4d019a8b
x7mooDx <41098832+xGameMC@users.noreply.github.com>
2018-08-05 10:49:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -307,7 +307,7 @@ client.on('message', message => {
     .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true
     .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true             
     .setFooter(`Morro Bot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
-    .setThumbnail(heg.avatarURL);
+    .setThumbnail(heg.avatarURL)
     message.channel.send(id)
 }       });
 	
